8dfd192108f373a81325a68768efeccbc2dfd5f0
Isela <isela.v.alarcon@gmail.com>
2021-05-06 16:29:18 +0200

    Reminder on Event Handler

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,18 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "dkfkdl" };
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input type="text" value={this.state.term} />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
