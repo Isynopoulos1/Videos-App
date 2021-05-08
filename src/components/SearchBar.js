e1bdfe205f453748f824e03b6cbd05bc6c628113
Isela <isela.v.alarcon@gmail.com>
2021-05-08 23:58:05 +0200

    testing searchbar state

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  state = { term: "" };
+  state = { term: "ksdjsjd" };
 
   OnInputChange = event => {
     this.setState({ term: event.target.value });
