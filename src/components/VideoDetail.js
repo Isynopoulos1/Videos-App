4f7fbe0029f56809961704b481f32714f9ad9664
Isela <isela.v.alarcon@gmail.com>
2021-05-15 19:16:09 +0200

    Displaying a video play

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -9,7 +9,7 @@ const VideoDetail = ({ video }) => {
   return (
     <div>
       <div className="ui embed">
-        <iframe src={videoSrc} />
+        <iframe title="video player" src={videoSrc} />
       </div>
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
