09259e34c3e5e26d64f8c5efa9a270ca973000ad
Isela <isela.v.alarcon@gmail.com>
2021-05-17 23:59:03 +0200

    Defaulting video selection

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,20 @@ import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit("moderat");
+  }
   onTermSubmit = async term => {
     const response = await youtube.get("/search", {
       params: {
         q: term
       }
     });
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
   };
 
   onVideoSelect = video => {
